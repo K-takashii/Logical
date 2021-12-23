96493b012ce612417a396c59ea8d42ae5af0d0fb
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-12-23 15:05:31 +0900

    Initial commit

---
+++ Logical_02.java
@@ -0,0 +1,9 @@
+class Logical_02 {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 3;
+
+        System.out.println(x == 3 || y == 3);
+        System.out.println(x == 1 || y == 1);
+    }
+}
